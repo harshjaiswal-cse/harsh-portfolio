18a981c366053edf03d0fe7bb60455a30bf5cec4
harsh kumar jaiswal <harshjaiswal8953@gmail.com>
2026-01-26 00:05:11 +0530

    update resume because add new project in resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
         </div>
         <div className="hero-resume">
           {/* Tumhara asali resume link connect kar diya hai */}
-          <a href="https://drive.google.com/file/d/1T3FAi75dlo-YAhyurHmTaPMpjdG9evNB/view?usp=drive_link" target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none', color: 'white'}}>
+          <a href="https://drive.google.com/file/d/1Nf95YPpc2K7SrqBcmN1hYUWTVCYnwNXt/view?usp=drive_link" target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none', color: 'white'}}>
             My resume
           </a>
         </div>
